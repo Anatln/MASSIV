4d88a1f9fcc28dcc266acc4a013e2b130af756c4
student <student@student-2>
2016-09-05 11:11:02 +0300

    Конец 4ГО урока.

---
+++ src/massiv/MASSIV.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package massiv;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author student
+ */
+public class MASSIV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int[] x;
+        x = new int[5];
+        x[0] = 23;
+        x[1] = 56;
+        x[2] = 31;
+        x[3] = 68;
+        x[4] = 5;
+           
+        
+        
+//        int[] y = {2, 3, 5, 7, 8};
+//
+//        for (int i = 0; i < x.length; i++) {
+//            System.out.println(x[i]);
+//        }
+//        int xmax = x[0];
+//        int xmin = x[0];
+//        int sum = 0;
+//        double avg;
+//        for (int j = 0; j < x.length; j++) {
+//            if (x[j] > xmax) {
+//                xmax = x[j];
+//            }
+//            if (x[j] < xmin) {
+//                xmin = x[j];
+//            }
+//            sum = sum + x[j];
+//            if (x[j] % 2 == 1) {
+//                System.out.print(x[j] + ", ");
+//
+//            }
+//
+//        }
+//
+//        avg = (double) sum / x.length;
+//        System.out.println("X max = " + xmax);
+//        System.out.println("X min = " + xmin);
+//        System.out.println("SUM = " + sum);
+//        System.out.println("AVG = " + avg);
+//        
+//            int[] z = new int[5];        
+//    Scanner sn = new Scanner(System.in);
+//
+//   for (int t=0; t<5; t++)
+//        { 
+//            System.out.println("Введите" + t + "-й массива: ");
+//
+//         z[t] = sn.nextInt();
+//        System.out.println();
+//    }
+//        
+   
+
+        
+    }
+
+
+}
